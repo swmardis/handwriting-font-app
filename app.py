efc8b7c4d803904ad77bc144ccdd9bfb907839a5
swmardis <stevenmardis@gmail.com>
2025-07-24 00:05:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,121 +2,31 @@ import streamlit as st
 import numpy as np
 import cv2
 from PIL import Image
-from fontTools.ttLib import TTFont, newTable
+from fontTools.ttLib import TTFont
 from fontTools.pens.ttGlyphPen import TTGlyphPen
-from fontTools.ttLib.tables import _m_a_x_p
 import io
 
 st.set_page_config(layout="wide")
 st.title("✍️ Handwriting to Font — Label & Generate")
 
-def create_minimal_blank_font():
-    font = TTFont()
+def create_font_from_template(template_path="template_font.ttf"):
+    font = TTFont(template_path)
 
-    font.setGlyphOrder(['.notdef'])
+    # Remove all glyphs except .notdef
+    glyphs = font['glyf'].glyphs
+    for g in list(glyphs.keys()):
+        if g != '.notdef':
+            del glyphs[g]
 
-    # Initialize tables except maxp
-    for tag in ['head', 'hhea', 'name', 'OS/2', 'post', 'cmap', 'glyf', 'hmtx']:
-        font[tag] = newTable(tag)
+    # Clear cmap subtables
+    for table in font['cmap'].tables:
+        table.cmap.clear()
 
-    # Properly initialize maxp table from its class
-    maxp = _m_a_x_p.table__m_a_x_p()
-    maxp.tableVersion = 1.0
-    maxp.numGlyphs = 1
-    maxp.maxPoints = 0
-    maxp.maxContours = 0
-    maxp.maxCompositePoints = 0
-    maxp.maxCompositeContours = 0
-    maxp.maxZones = 1
-    maxp.maxTwilightPoints = 0
-    maxp.maxStorage = 0
-    maxp.maxFunctionDefs = 0
-    maxp.maxInstructionDefs = 0
-    maxp.maxStackElements = 0
-    maxp.maxSizeOfInstructions = 0
-    maxp.maxComponentElements = 0
-    maxp.maxComponentDepth = 0
-    font['maxp'] = maxp
-
-    # head table minimal required fields
-    font['head'].unitsPerEm = 1000
-    font['head'].xMin = 0
-    font['head'].yMin = 0
-    font['head'].xMax = 1000
-    font['head'].yMax = 1000
-    font['head'].flags = 0
-    font['head'].created = 0
-    font['head'].modified = 0
-    font['head'].macStyle = 0
-    font['head'].lowestRecPPEM = 8
-    font['head'].fontDirectionHint = 2
-    font['head'].magicNumber = 0x5F0F3CF5
-    font['head'].indexToLocFormat = 0
-    font['head'].glyphDataFormat = 0
-
-    # hhea table
-    font['hhea'].ascent = 800
-    font['hhea'].descent = -200
-    font['hhea'].lineGap = 0
-    font['hhea'].numberOfHMetrics = 1
-    font['hhea'].advanceWidthMax = 1000
-    font['hhea'].minLeftSideBearing = 0
-    font['hhea'].minRightSideBearing = 0
-    font['hhea'].xMaxExtent = 1000
-    font['hhea'].caretSlopeRise = 1
-    font['hhea'].caretSlopeRun = 0
-    font['hhea'].caretOffset = 0
-    font['hhea'].reserved0 = 0
-    font['hhea'].reserved1 = 0
-    font['hhea'].reserved2 = 0
-    font['hhea'].reserved3 = 0
-
-    # name table empty list (you can add metadata here later)
-    font['name'].names = []
-
-    # OS/2 minimal
-    font['OS/2'].usFirstCharIndex = 0
-    font['OS/2'].usLastCharIndex = 0
-    font['OS/2'].sTypoAscender = 800
-    font['OS/2'].sTypoDescender = -200
-    font['OS/2'].usWinAscent = 800
-    font['OS/2'].usWinDescent = 200
-    font['OS/2'].fsSelection = 0
-    font['OS/2'].panose = b'\x00'*10
-    font['OS/2'].achVendID = 'NONE'
-
-    # post table minimal
-    font['post'].formatType = 3.0
-    font['post'].underlinePosition = 0
-    font['post'].underlineThickness = 0
-    font['post'].isFixedPitch = 0
-    font['post'].minMemType42 = 0
-    font['post'].maxMemType42 = 0
-    font['post'].minMemType1 = 0
-    font['post'].maxMemType1 = 0
-
-    # cmap table with valid format 4 subtable
-    from fontTools.ttLib.tables._c_m_a_p import cmap_format_4
-    cmap_subtable = cmap_format_4(4)
-    cmap_subtable.platformID = 3
-    cmap_subtable.platEncID = 1
-    cmap_subtable.language = 0
-    cmap_subtable.cmap = {}
-    font['cmap'].tables = [cmap_subtable]
-
-    # Initialize glyf and hmtx tables
-    font['glyf'].glyphs = {}
-    font['hmtx'].metrics = {}
-
-    # Add empty .notdef glyph
-    pen = TTGlyphPen(None)
-    pen.moveTo((0, 0))
-    pen.lineTo((0, 0))
-    pen.closePath()
-    glyph = pen.glyph()
-    font['glyf'].glyphs['.notdef'] = glyph
-
-    font['hmtx'].metrics['.notdef'] = (0, 0)
+    # Reset hmtx metrics except .notdef
+    hmtx_metrics = font['hmtx'].metrics
+    keys_to_remove = [k for k in hmtx_metrics if k != '.notdef']
+    for key in keys_to_remove:
+        del hmtx_metrics[key]
 
     return font
 
@@ -145,8 +55,8 @@ def create_glyph(width, height):
     pen.closePath()
     return pen.glyph()
 
-def make_font(char_images, char_labels):
-    font = create_minimal_blank_font()
+def make_font(char_images, char_labels, template_path="template_font.ttf"):
+    font = create_font_from_template(template_path)
     font.setGlyphOrder(['.notdef'] + char_labels)
 
     glyf = font['glyf']
